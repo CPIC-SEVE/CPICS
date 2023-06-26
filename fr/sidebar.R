7b1b426b43d99aab2d26d458db343e9a5ded68e8
KarenGoncalves <cris.kgs@gmail.com>
2023-06-26 08:32:13 -0400

    correcting hover color for insta link

--- fr/sidebar.R
+++ fr/sidebar.R
@@ -3,7 +3,7 @@ cat('<center><span> <p class="sidebar_logos">[<i class="fab fa-twitter"></i>](ht
 '[<i class="fab fa-linkedin-in"></i>](https://www.linkedin.com/company/cpics/about/)',
 '[<i class="fab fa-facebook"></i>](https://www.facebook.com/CPICS-Comit%C3%A9-de-partenariat-international-du-Centre-S%C3%88VE-395275957711442)', 
 '<a  href = "mailto:cpicseve@gmail.com"><i class="fas fa-envelope" align="center" style="font-size:24px"></i></a>',
-'<a href="https://www.instagram.com/cpics2023/"><i class="fa-brands fa-instagram" style="color: #00a300;"></i></a>', 
+'[<i class="fa-brands fa-instagram"></i>](https://www.instagram.com/cpics2023/)', 
 '</p></span></center>\n\n')
 cat('<div class="row" style="margin: 1%">\n\n')
 cat('<a href="http://centreseve.recherche.usherbrooke.ca/fr"><img src="images/logos/centre_seve.webp" alt="Centre SEVE logo" width="50%"></a><a href="http://www.frqnt.gouv.qc.ca/en/accueil"><img  src="images/logos/FRQNT_blanc(transparent).webp" width="50%"></a>\n\n')
